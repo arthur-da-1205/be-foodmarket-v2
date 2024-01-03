f10dd305d7c263cda9dcb5217bce87dae7640f40
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2024-01-03 14:55:09 +0700

    upload image module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CloudinaryService } from './cloudinary/cloudinary.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    CloudinaryModule,
+  ],
+  providers: [CloudinaryService],
 })
 export class AppModule {}
