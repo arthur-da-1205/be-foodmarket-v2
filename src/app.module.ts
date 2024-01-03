aead8c8c01d510f28616a62b7402613540a34d4b
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2024-01-03 16:31:27 +0700

    Get Profile and Update Profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { CloudinaryService } from './cloudinary/cloudinary.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,7 +14,9 @@ import { PrismaModule } from './prisma/prisma.module';
     PrismaModule,
     AuthModule,
     CloudinaryModule,
+    UserModule,
   ],
-  providers: [CloudinaryService],
+  providers: [CloudinaryService, UserService],
+  controllers: [UserController],
 })
 export class AppModule {}
